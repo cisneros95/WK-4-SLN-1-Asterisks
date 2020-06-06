e8f9d216a64e8d6a5f677aec0b51d86e4179d8a2
Raul Cisneros <raul.f.cisneros@outlook.com>
2020-06-06 09:20:30 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_4_WK_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayAstrick(); // Calling the method
+        }
+
+        static void DisplayAstrick()
+        {
+            string star = "*";
+            char space = ' ';
+            int index = 1;
+            int counter = 0;
+
+
+            Console.Write("Input Number of stars: "); // Ask the user to input number 
+            int a = Convert.ToInt32(Console.ReadLine());  // 
+            Console.WriteLine();
+
+            while (index <= a)
+            {
+                while (counter <= a)
+                {
+                    Console.Write(star);
+                    counter++;
+                   
+
+                }
+            }
+        }
+    }
+}
